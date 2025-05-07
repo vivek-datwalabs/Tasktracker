92412a18d787f3d13909ef302d592aef5d074407
vivekranjansahoo <vivekranjansahoo81@gmail.com>
2025-05-07 17:52:47 +0530

    ✅ Add: Logger

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-import taskRoutes from './routes/task.routes.js';
-import { errorMiddleware } from './middlewares/error.middleware.js';
+import morgan from "morgan";
+import dayjs from "dayjs";
+import dotenv from "dotenv";
+import taskRoutes from "./routes/task.routes.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +12,12 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Custom Morgan format: logs time, method, URL, status, response time
+morgan.token("date", () => dayjs().format("YYYY-MM-DD HH:mm:ss"));
+const logFormat = '[:date] ":method :url" :status - :response-time ms';
+
+app.use(morgan(logFormat));
 
 // Routes
 app.use('/api/tasks', taskRoutes);
